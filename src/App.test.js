d916860d800aa9008da1bff91459f49999314cda
peteryunkim <peteryunkim@gmail.com>
2017-08-28 15:44:38 -0400

    added 3rd test to test text to include counter

--- src/App.test.js
+++ src/App.test.js
@@ -4,9 +4,15 @@ import App from './App';
 
 describe('App Component', () => {
 	it('renders App component properly', () => {
-		let aComponent = shallow(<App />)
-		expect(aComponent).toHaveLength(1)
+		let component = shallow(<App />)
+		expect(component).toHaveLength(1)
 	})
+	it('displays count', () => {
+		let component = shallow(<App/>)
+		// console.log(component.find('h2').text())
+		expect(component.find('h2').text()).toEqual('Count: 0')
+	})
+
 	describe('button', () => {
 		it('when clicked, it increases count', () => {
 			let component = shallow(<App/>)
