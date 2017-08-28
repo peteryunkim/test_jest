4be4d6db7757134b0156128af43ca3f37c55852c
peteryunkim <peteryunkim@gmail.com>
2017-08-28 15:26:40 -0400

    added 2nd test to test counter

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import './App.css';
 
 class App extends Component {
 
+  constructor(){
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
 
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={this.increment}>Increase Count </button>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -7,4 +7,12 @@ describe('App Component', () => {
 		let aComponent = shallow(<App />)
 		expect(aComponent).toHaveLength(1)
 	})
+	describe('button', () => {
+		it('when clicked, it increases count', () => {
+			let component = shallow(<App/>)
+			let button = component.find('button')
+			button.simulate('click')
+			expect(component.state().count).toEqual(1)
+		})
+	})
 })
